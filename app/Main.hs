bf1507cd39347ab208abd72ac52e92015f24e584
Stefan Groth <stefan.groth@outlook.com>
2021-09-02 22:20:12 +0200

    Gloss window that draws white circles on click

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,50 @@
-module Main where
-
 import Lib
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
+
+width :: Int
+width = 300
+height :: Int
+height = 300
+offset :: Int
+offset = 100
+
+
+window :: Display
+window = InWindow "My Window" (width, height) (offset, 0)
+
+background :: Color
+background = black
+
+newtype Points = Points 
+    {
+        locs :: [Point]
+    } deriving Show
+
+render :: Points -> Picture
+render points =
+    pictures $ map mkCircle $ locs points
+    where        
+        mkCircle :: Point -> Picture
+        mkCircle (x, y) = translate x y $ color white $ circleSolid 10
+
+
+initialState :: Points
+initialState = Points { 
+    locs = []    
+}
+
+fps :: Int
+fps = 60
+
+handleKeys :: Event -> Points -> Points
+handleKeys (EventKey (MouseButton LeftButton) Down _ (x, y)) points =
+    points { locs = (x,y) : locs points }
+handleKeys _ game = game
+
 
 main :: IO ()
-main = someFunc
+main = play window background fps initialState render handleKeys update 
+    where 
+        update _ points = points
