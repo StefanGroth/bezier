92834e33a328843f8b838182e8478e1986902fe2
Stefan Groth <stefan.groth@outlook.com>
2021-09-03 17:40:12 +0200

    Recursive bezier and started on animation
    * Implemented a recursive defenition for the bezier curve in dependence
    of t
    * Used Maybe Monad to implement future animation of bezier curve.
    Currently this draws the last clicked circle as red.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
+import qualified Graphics.Gloss.Data.Point.Arithmetic as Arith
+import Control.Applicative
+
 width :: Int
 width = 300
 height :: Int
@@ -17,32 +20,47 @@ window = InWindow "My Window" (width, height) (offset, 0)
 background :: Color
 background = black
 
-newtype Points = Points 
+data Points = Points 
     {
-        locs :: [Point]
+        locs :: [Point],
+        anim :: Maybe Point 
     } deriving Show
 
 render :: Points -> Picture
 render points =
-    pictures $ map mkCircle $ locs points
+    pictures $ map mkCircle (locs points) ++ showAnim (anim points)
     where        
         mkCircle :: Point -> Picture
         mkCircle (x, y) = translate x y $ color white $ pictures [circleSolid 10, mkText x y]
         mkText x y = translate 15 (-5) $ scale 0.15 0.15 $ Text ("(" ++ show x ++ ", " ++ show y ++ ")")
-
+        
+        showAnim :: Maybe Point -> [Picture] 
+        showAnim Nothing = []
+        showAnim (Just (x, y)) = [translate x y $ color red $ circleSolid 10]
 
 initialState :: Points
 initialState = Points { 
-    locs = []    
+    locs = [],
+    anim = Nothing 
 }
+
+
+bezier :: [Point] -> Float -> Maybe Point 
+bezier [] _ = Nothing 
+bezier [point] _ = Just point 
+bezier points t = (Arith.+) <$> first <*> last
+    where   first = ((1-t) Arith.*) <$> bezier (init points) t
+            last = (t Arith.*) <$> bezier (tail points) t
 
 fps :: Int
 fps = 60
 
 handleKeys :: Event -> Points -> Points
 handleKeys (EventKey (MouseButton LeftButton) Down _ (x, y)) points =
-    points { locs = (x,y) : locs points }
+    points { locs = (x,y) : locs points, anim = Just (x, y) }
 handleKeys _ game = game
+
+
 
 
 main :: IO ()
